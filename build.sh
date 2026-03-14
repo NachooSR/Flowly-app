7d5f50464e5dbd93285f789bf0ce173eb30d148a
Reyna Ignacio Nicolas <nachosilvareyna@gmail.com>
2026-03-14 18:50:54 -0300

    Adding styles

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -o errexit
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
+
+cd src
 
 python manage.py collectstatic --no-input
 python manage.py migrate
--- src/config/settings.py
+++ src/config/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = "users.CustomUser"
 
@@ -132,5 +134,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 if not DEBUG:
-    STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
